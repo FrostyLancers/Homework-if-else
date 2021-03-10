c82b8b04a085dd8d73f22841f4623d19be7f8800
DESKTOP-KC8155E\Lenovo <EasterEggs1230@gmail.com>
2021-03-10 14:35:51 +0700

    Homework if-else 06.03.21

---
+++ Program (Homework  if-else 06.03.21).cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Homework__if_else_06._03._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            double mousex = double.Parse(Console.ReadLine());
+            double mousey = double.Parse(Console.ReadLine());
+
+            if (mode == "time" && mousex >= 0)
+            {
+                mousey = Math.Pow((mousex - 1), 2);
+                Console.WriteLine("{0}, {1}", mousex, mousey);
+            }
+
+            else if (mode == "price" && mousex >= 0)
+            {
+                mousex = Math.Sqrt(mousey) + 1;
+                Console.WriteLine("{0}, {1}", mousex, mousey);
+            }
+
+            else
+            {
+                if (mousex < 0)
+                {
+                    Console.WriteLine("Invalid Mouse Position.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Mode.");
+                }
+            }
+        }
+    }
+}
